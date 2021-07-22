d7c5283fc1f2c7ba8770ed1ad0ce4828d444fbb6
basuabhirup <basu.abhirup@yahoo.com>
2021-07-22 22:30:02 +0530

    Completed Project.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("Hello");
+for (var i = 0; i < document.querySelectorAll("input").length; i++) {
+  document.querySelectorAll("input")[i].addEventListener("mousemove", changeProperties);
+  document.querySelectorAll("input")[i].addEventListener("change", changeProperties);
+}
+
+function changeProperties() {
+  var suffix = this.dataset.sizing || ""; // to set the px unit wherever applicable
+  var root = document.querySelector(":root");
+  root.style.setProperty("--" + this.name , (this.value + suffix));
+}
